4625cb927f09bcbff36999f856c692b4aac2fc04
srinivasarao <srinivasaraouesi@gmail.com>
2017-12-27 19:21:30 +0530

    first commit

---
+++ app/src/main/java/com/srinu/bottomsheets/CustomBottomSheetDialogFragment.java
@@ -0,0 +1,21 @@
+package com.srinu.bottomsheets;
+
+/**
+ * Created by ConquerorsTechnologies on 27/12/17.
+ */
+
+import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_dialog_bottom_sheet, container, false);
+        return v;
+    }
+
+
+}
